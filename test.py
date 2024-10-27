dffd9dc650fbfb02432d61cdd35662a3cd3837f3
Deadmockey <deadmockey@protonmail.com>
2024-10-27 14:25:39 -0500

    commit

--- test.py
+++ test.py
@@ -3,12 +3,24 @@ import discogs_client
 import os
 
 
+def user_input(client: discogs_client.Client):
+    user_search_query = input("Enter in the name of the album: ")
+
+    serach_results = client.search(user_search_query)
+    
+    print("\nDisplaying top 5 results for your search:")
+
+    for i in range(5):
+        print(serach_results.page(1)[i].title)
+
+    user_selection = input("Enter the number of release")
+
 def main():
     load_dotenv()
     TOKEN = os.environ["TOKEN"]
     client = discogs_client.Client("TestApp", user_token=TOKEN)
-    test_search = client.search("luv(sic)")
-    print(test_search[0].artists[0])
+
+    user_search = user_input(client)
 
 
 main()
